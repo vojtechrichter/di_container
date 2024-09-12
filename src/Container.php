a7b52484228f55bad9506cd1e37b3c7c3d041076
vojtechrichter <vojta3218@gmail.com>
2024-09-12 12:21:24 +0200

    changed services values to custom callables

--- src/Container.php
+++ src/Container.php
@@ -3,29 +3,42 @@
 namespace VojtechRichter\DiContainer;
 
 use Psr\Container\ContainerInterface;
+use VojtechRichter\DiContainer\Exceptions\ServiceClassDoesNotExistException;
 use VojtechRichter\DiContainer\Exceptions\UnregisteredContainerIdentifierException;
 
 final class Container implements ContainerInterface
 {
-    /** @var array<string, string> $services */
+    /** @var array<string, callable> $services */
     private array $services = [];
+    private bool $autowiring_enabled = false;
 
     public function __construct()
     {
     }
 
-    public function set(string $id, string $service): void
+    public function set(string $id, callable $service): void
     {
         $this->services[$id] = $service;
     }
 
-    public function get(string $id)
+    public function enableAutowiring(): void
+    {
+        $this->autowiring_enabled = true;
+    }
+
+    public function get(string $id): mixed
     {
         if (!array_key_exists($id, $this->services)) {
+//            if ($this->autowiring_enabled) {
+//                if (class_exists($id)) {
+//
+//                }
+//                throw new ServiceClassDoesNotExistException($id);
+//            }
             throw new UnregisteredContainerIdentifierException($id);
         }
 
-        return Service::init($this->services[$id]);
+        return call_user_func($this->services[$id]);
     }
 
     public function has(string $id): bool
